e4ce7aad4797102e7b1657aa0be7fed4bc56d02f
admin <m-gulin@ya.ru>
2025-04-18 17:19:00 +0300

    Изменен server

--- server.py
+++ server.py
@@ -51,7 +51,7 @@ def get_cities(req):
     cities = []
     for entity in req['request']['nlu']['entities']:
         if entity['type'] == 'YANDEX.GEO':
-            if 'city' in entity['value']:
+            if 'city' in entity['value'].keys():
                 cities.append(entity['value']['city'])
     return cities
 
