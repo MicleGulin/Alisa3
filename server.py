8233d2ab1e8316d72aa9d6b1d83ab569434a40b8
admin <m-gulin@ya.ru>
2025-04-26 18:56:20 +0300

    Исправлено 1

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
-import logging, os, requests
+import logging, os, random
+from geocoder import get_distance, get_geo_info
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
+sessionStorage = {}
 
 
 @app.route('/post', methods=['POST'])
@@ -20,19 +22,46 @@ def main():
 
 
 def processing_dialog(req, res):
+    user_id = req['session']['user']['user_id']
+
     if req['session']['new']:
-        res['response'][
-            'text'] = 'Привет, я умею переводить слова с русского на английский, только скажи: "Переведи слово: <слово>".'
+        sessionStorage[user_id] = {'name': None}
+        res['response']['text'] = 'Какое у тебя имя?'
     else:
-        if 'request' in req and 'original_utterance' in req['request']:
-            text = req['request']['original_utterance']
-            if 'Переведи слово:' in text or 'Переведите слово:' in text:
-                text = text.split(':')[1]
-                response = requests.post('https://api.mymemory.translated.net/get',
-                                         data={'q': text, 'langpair': 'ru|en'})
-                res['response']['text'] = response.json()['responseData']['translatedText']
+        if sessionStorage[user_id]['name']:
+            city = get_city(req)
+            if not city:
+                res['response']['text'] = f'{sessionStorage[user_id]['name']}, ты не написал название ни одного города!'
+            elif len(city) == 1:
+                res['response'][
+                    'text'] = f'{sessionStorage[user_id]['name']}, этот городе в стране {get_geo_info(city[0], 'country')}.'
+            elif len(city) == 2:
+                res['response'][
+                    'text'] = f'{sessionStorage[user_id]['name']}, расстояние между городами {round(get_distance(get_geo_info(city[0], 'coordinates'), get_geo_info(city[1], 'coordinates')))}м.'
             else:
-                res['response']['text'] = 'Нет такой команды'
+                res['response']['text'] = f'{sessionStorage[user_id]['name']}, слишком много городов. Я запуталась'
+        else:
+            name = get_name(req)
+            if name:
+                sessionStorage[user_id]['name'] = name.capitalize()
+                res['response'][
+                    'text'] = f'Привет, {sessionStorage[user_id]['name']}! Я могу показать город или сказать расстояние между городами! Только напиши название или названия.'
+            else:
+                res['response']['text'] = 'Я не расслышала, можешь повторить?'
+
+
+def get_city(req):
+    cities = []
+    for i in req['request']['nlu']['entities']:
+        if i['type'] == 'YANDEX.GEO' and 'city' in i['value']:
+            cities.append(i['value']['city'])
+    return cities
+
+
+def get_name(req):
+    for i in req['request']['nlu']['entities']:
+        if i['type'] == 'YANDEX.FIO':
+            return i['value'].get('first_name', None)
 
 
 if __name__ == '__main__':
